588c7234e3327322ed71910513202ad1a0ff2cac
pavedom13 <pavedom13@gmail.com>
2020-11-30 12:31:09 +0300

    section route без адаптива

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(function(){
-    $('.about__slider').slick({
+    $('.about__slider,.route__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./image/content/slider-about/prev-about.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./image/content/slider-about/next-about.svg" alt="bext"></button>',
         autoplay: true,//пролистывание по умолчнию 3 сек
         fade: true// эффект возникновения слайда вместо перелистывания
     });
 });
+
+
+// $(function(){
+//     $('.route-slider').slick({
+//         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./image/content/slider-about/prev-about.svg" alt="prev"></button>',
+//         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./image/content/slider-about/next-about.svg" alt="bext"></button>',
+//         autoplay: true,//пролистывание по умолчнию 3 сек
+//         fade: true// эффект возникновения слайда вместо перелистывания
+//     });
+// });
