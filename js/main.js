dd93223c685148435fa0a3f58f9346fbb57a1fd2
pavedom13 <pavedom13@gmail.com>
2020-11-30 12:42:25 +0300

    исправления в section  route

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.about__slider,.route__slider').slick({
+    $('.about-slider,.route-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./image/content/slider-about/prev-about.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./image/content/slider-about/next-about.svg" alt="bext"></button>',
         autoplay: true,//пролистывание по умолчнию 3 сек
