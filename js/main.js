6229c1a0fd853db51eaba12e712685e9d5b22390
pavedom13 <pavedom13@gmail.com>
2020-12-03 20:20:09 +0300

    адаптив слайдеров готово

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-$(function(){
+$(function () {
     $('.about-slider,.route-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./image/content/slider-about/prev-about.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./image/content/slider-about/next-about.svg" alt="bext"></button>',
         autoplay: true,//пролистывание по умолчнию 3 сек
-        fade: true// эффект возникновения слайда вместо перелистывания
+        fade: true,// эффект возникновения слайда вместо перелистывания
+        responsive: [
+            {
+                breakpoint: 601,//на ширине меньше 601 стрелок не будет
+                settings: {
+                    arrows: false
+                }
+            },
+        ]
     });
 });
 
